2a3d817d47f861078be9804d97304236193f092e
Marie Otaki <otakimarie@gmail.com>
2019-10-14 18:58:10 +0700

    make the map show snowy district

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import mapboxgl from "mapbox-gl";
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const BASE_URL = "https://api.mapbox.com/styles/v1/mapbox/streets-v9";
+
+class App extends Component {
+  state = {
+    style: false
+  };
+
+  componentDidMount = async () => {
+    const url = `${BASE_URL}?access_token=${mapboxgl.accessToken}`;
+
+    let style = {};
+    try {
+      style = await fetch(url).then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("error");
+        }
+      });
+    } catch (e) {
+      return;
+    }
+
+    this.map = new mapboxgl.Map({
+      container: this.container,
+      style
+    });
+
+    this.setState({ style });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.style !== this.state.style) {
+      this.map.setStyle(this.state.style);
+    }
+  }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
+  onClick = () => {
+    const prevStyle = this.state.style;
+    console.log(prevStyle);
+    const nextStyle = {
+      ...prevStyle,
+      layers: prevStyle.layers.map(layer =>
+        layer.id === "landcover_snow"
+          ? { ...layer, paint: { ...layer.paint, "fill-color": "red" } }
+          : layer
+      )
+    };
+    this.setState({ style: nextStyle });
+  };
+
+  render() {
+    return (
+      <div className={"map"} ref={e => (this.container = e)}>
+        <button
+          style={{ position: "absolute", zIndex: 1 }}
+          onClick={this.onClick}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {"氷雪地帯は？"}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import mapboxgl from "mapbox-gl";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFyaWUtd29xIiwiYSI6ImNrMXFiM2ppbDAxOHczZHN4aTduNWUxZWYifQ.sPd9Rb9AnUACVGAT8S7t4A"; // Mapbox access token
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
