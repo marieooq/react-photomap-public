512ffd062a49c97ac37249d2a7e8903ff09e616b
Marie Otaki <otakimarie@gmail.com>
2019-10-24 17:26:36 +0700

    add animate a point along a route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Geocode from "react-geocode";
 import "./App.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import axios from "axios";
+// import { greatCircle, point } from "@turf/turf";
+// // Alternatively you can import the whole lot using
+import * as turf from "@turf/turf";
 
 const BASE_URL = "https://api.mapbox.com/styles/v1/mapbox/streets-v9";
 
@@ -57,10 +60,10 @@ class App extends Component {
 
     this.map.on("load", () => {
       this.state.dataFromTwitter.forEach((data, index) => {
-        console.log("This is data from twitter API");
-        console.log("-----");
-        console.log(data.text);
-        console.log("-----");
+        // console.log("This is data from twitter API");
+        // console.log("-----");
+        // console.log(data.text);
+        // console.log("-----");
         const truncateCreatedDate = () => {
           const date = data.created_at.substring(4, 10);
           const year = data.created_at.substring(26, 30);
@@ -158,6 +161,168 @@ class App extends Component {
           }
         );
       });
+
+      ///////////////////addition///////////////////
+      // San Francisco
+      const origin = [-122.414, 37.776];
+
+      // Washington DC
+      const destination = [-77.032, 38.913];
+
+      // A simple line from origin to destination.
+      const route = {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            geometry: {
+              type: "LineString",
+              coordinates: [origin, destination]
+            }
+          }
+        ]
+      };
+
+      // A single point that animates along the route.
+      // Coordinates are initially set to origin.
+      const point = {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "Point",
+              coordinates: origin
+            }
+          }
+        ]
+      };
+
+      console.log("---");
+      console.log(turf);
+      console.log("---");
+      // Calculate the distance in kilometers between route start/end point.
+      const lineDistance = turf.lineDistance(route.features[0], {
+        units: "kilometers"
+      });
+
+      let arc = [];
+
+      // Number of steps to use in the arc and animation, more steps means
+      // a smoother arc and animation, but too many steps will result in a
+      // low frame rate
+      const steps = 500;
+
+      // Draw an arc between the `origin` & `destination` of the two points
+      for (let i = 0; i < lineDistance; i += lineDistance / steps) {
+        const segment = turf.along(route.features[0], i, {
+          units: "kilometers"
+        });
+        arc.push(segment.geometry.coordinates);
+      }
+
+      // Update the route with calculated arc coordinates
+      route.features[0].geometry.coordinates = arc;
+
+      // Used to increment the value of the point measurement against the route.
+      let counter = 0;
+
+      // Add a source and layer displaying a point which will be animated in a circle.
+      this.map.addSource("route", {
+        type: "geojson",
+        data: route
+      });
+
+      this.map.addSource("point", {
+        type: "geojson",
+        data: point
+      });
+
+      this.map.addLayer({
+        id: "route",
+        source: "route",
+        type: "line",
+        paint: {
+          "line-width": 2,
+          "line-color": "#007cbf"
+        }
+      });
+
+      this.map.addLayer({
+        id: "point",
+        source: "point",
+        type: "symbol",
+        layout: {
+          "icon-image": "airport-15",
+          "icon-rotate": ["get", "bearing"],
+          "icon-rotation-alignment": "map",
+          "icon-allow-overlap": true,
+          "icon-ignore-placement": true
+        }
+      });
+
+      const animate = () => {
+        // Update point geometry to a new position based on counter denoting
+        // the index to access the arc.
+        point.features[0].geometry.coordinates =
+          route.features[0].geometry.coordinates[counter];
+
+        // Calculate the bearing to ensure the icon is rotated to match the route arc
+        // The bearing is calculate between the current point and the next point, except
+        // at the end of the arc use the previous point and the current point
+
+        console.log("-----");
+        console.log(point.features[0].properties.bearing);
+        console.log("-----");
+
+        point.features[0].properties.bearing = turf.bearing(
+          turf.point(
+            route.features[0].geometry.coordinates[
+              counter >= steps ? counter - 1 : counter
+            ]
+          ),
+          turf.point(
+            route.features[0].geometry.coordinates[
+              counter >= steps ? counter : counter + 1
+            ]
+          )
+        );
+
+        // Update the source with this new data.
+        this.map.getSource("point").setData(point);
+
+        // Request the next frame of animation so long the end has not been reached.
+        if (counter < steps - 2) {
+          console.log(`This is steps ${steps}`);
+          console.log(`This is counter ${counter}`);
+          requestAnimationFrame(animate);
+        } else {
+          console.log("end");
+          return;
+        }
+
+        counter = counter + 1;
+      };
+
+      document.getElementById("replay").addEventListener("click", function() {
+        // Set the coordinates of the original point back to origin
+        point.features[0].geometry.coordinates = origin;
+
+        // Update the source layer
+        this.map.getSource("point").setData(point);
+
+        // Reset the counter
+        counter = 0;
+
+        // Restart the animation.
+        animate(counter);
+      });
+
+      // Start the animation.
+      animate(counter);
+
+      ///////////////////addition///////////////////
     }); //inside on "load"
   };
 
@@ -175,6 +340,9 @@ class App extends Component {
     return (
       <div>
         <div className={"map"} ref={e => (this.container = e)}></div>
+        <div class="overlay">
+          <button id="replay">Replay</button>
+        </div>
       </div>
     );
   }
